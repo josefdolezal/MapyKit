9028feed4642132376542c3bf48dc0123d840e56
Josef Dolezal <pepik.ml@gmail.com>
2018-09-04 19:52:35 +0200

    Create base project structure

---
+++ MapyKit/MapyKit.h
@@ -0,0 +1,19 @@
+//
+//  MapyKit.h
+//  MapyKit
+//
+//  Created by Josef Dolezal on 04/09/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapyKit.
+FOUNDATION_EXPORT double MapyKitVersionNumber;
+
+//! Project version string for MapyKit.
+FOUNDATION_EXPORT const unsigned char MapyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapyKit/PublicHeader.h>
+
+
