8d79a873873b21e131970cc0b11ef95e6e92f6ee
Josef Dolezal <pepik.ml@gmail.com>
2018-09-16 15:25:40 +0200

    Create MapyAPI target

---
+++ MapyAPI/Supporting Files/MapyAPI.h
@@ -0,0 +1,19 @@
+//
+//  MapyAPI.h
+//  MapyAPI
+//
+//  Created by Josef Dolezal on 16/09/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapyAPI.
+FOUNDATION_EXPORT double MapyAPIVersionNumber;
+
+//! Project version string for MapyAPI.
+FOUNDATION_EXPORT const unsigned char MapyAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapyAPI/PublicHeader.h>
+
+
