757d372c04548038f71d224b198b64574b2bfb37
Josef Dolezal <pepik.ml@gmail.com>
2018-09-16 15:49:09 +0200

    Create FastRPC serialization protocol

---
+++ MapyAPI/FastRPC/FastRPCSerializable.swift
@@ -0,0 +1,19 @@
+//
+//  FastRPCSerializable.swift
+//  MapyAPI
+//
+//  Created by Josef Dolezal on 16/09/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+/// Object which is able to be serialized through FastRPC protocol.
+protocol FastRPCSerializable {
+    /// Returns raw data representing object in FastRPC protocol.
+    /// Throws FastRPC error if object is not serializable.
+    ///
+    /// - Returns: Raw data representing structure
+    /// - Throws: FastRPCError on failure
+    func serialize() throws -> Data
+}
