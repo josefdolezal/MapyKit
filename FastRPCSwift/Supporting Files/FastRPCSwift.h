6ec891c38b9cce416db66fabdd6a61226a4fd5d2
Josef Dolezal <pepik.ml@gmail.com>
2018-09-21 13:52:07 +0200

    Create separate target for FRPC

---
+++ FastRPCSwift/Supporting Files/FastRPCSwift.h
@@ -0,0 +1,19 @@
+//
+//  FastRPCSwift.h
+//  FastRPCSwift
+//
+//  Created by Josef Dolezal on 21/09/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastRPCSwift.
+FOUNDATION_EXPORT double FastRPCSwiftVersionNumber;
+
+//! Project version string for FastRPCSwift.
+FOUNDATION_EXPORT const unsigned char FastRPCSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastRPCSwift/PublicHeader.h>
+
+
