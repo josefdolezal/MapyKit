6c513921dd53b040d3783004b049e1c0ba2f081d
Josef Dolezal <pepik.ml@gmail.com>
2018-09-28 15:59:25 +0200

    Make Location interface public

--- MapyAPI/Model/Location.swift
+++ MapyAPI/Model/Location.swift
@@ -16,4 +16,16 @@ public struct Location {
     public var latitude: Double
     /// Coordinations longitude
     public var longitude: Double
+
+    // MARK: Initializers
+
+    /// Creates new coordinates with given latitude and longitude.
+    ///
+    /// - Parameters:
+    ///   - latitude: Coordinates latitude
+    ///   - longitude: Coordinates longitude
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
