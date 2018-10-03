8a7e45e7f27fb68130b9b8b4b71e3d149909641b
Josef Dolezal <pepik.ml@gmail.com>
2018-10-03 22:52:25 +0200

    Add tests String extension for random string

---
+++ FastRPCSwiftTests/Helpers/String+Random.swift
@@ -0,0 +1,22 @@
+//
+//  String+Random.swift
+//  FastRPCSwiftTests
+//
+//  Created by Josef Dolezal on 03/10/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func random(maxLength: Int) -> String {
+        let chars = (0...maxLength)
+            .map { _ -> Character in
+                let char = UInt8.random(in: .min ... .max)
+
+                return Character(Unicode.Scalar(char))
+        }
+
+        return String(chars)
+    }
+}
