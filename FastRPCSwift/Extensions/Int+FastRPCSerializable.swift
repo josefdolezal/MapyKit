b33220655675dbbc5e3bf99c411959c2ebafea4d
Josef Dolezal <pepik.ml@gmail.com>
2018-10-05 13:40:42 +0200

    Add Int convenience initializer from Data

--- FastRPCSwift/Extensions/Int+FastRPCSerializable.swift
+++ FastRPCSwift/Extensions/Int+FastRPCSerializable.swift
@@ -42,4 +42,24 @@ extension Int {
         // Advance used bits by seven so the division is rounded up
         return (nonTrailingBitsCount + 7) / 8
     }
+
+    /// Initializes Integer from raw data. Data must be little endian representation of integer and
+    /// must not contain more bytes that is used to represent integer.
+    init(data: Data) {
+        // Check if byte representation is convertible from given data
+        assert(MemoryLayout<Int>.size >= data.count)
+
+        // Convert data to Integer - discussion: we cannot use standard solution using memory layout,
+        // since it work only with properly aligned bytes (count matches with actual implementation)
+        // therefore we use bits shifting with sum
+        self = data
+            // Get each bytes offset
+            .enumerated()
+            // Shift bytes by it's offset
+            .map { offset, byte in
+                return Int(byte) << (offset * 8)
+            }
+            // Sum the bytes to get the actual value
+            .reduce(0, +)
+    }
 }
