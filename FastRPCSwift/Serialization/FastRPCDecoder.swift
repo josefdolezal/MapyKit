21b3ec5b07b038437a83c3c99dc1ee12b3997e41
Josef Dolezal <pepik.ml@gmail.com>
2018-10-11 12:31:41 +0200

    Use generics for FRPC single values decoding

--- FastRPCSwift/Serialization/FastRPCDecoder.swift
+++ FastRPCSwift/Serialization/FastRPCDecoder.swift
@@ -49,75 +49,46 @@ class _FastRPCDecoder: Decoder, SingleValueDecodingContainer {
         return container is NSNull
     }
 
-    func decode(_ type: Bool.Type) throws -> Bool {
-        guard let bool = container as? Bool else {
-            throw typeMismatchError(expcted: type, butGot: container)
-        }
-
-        return bool
-    }
-
-    func decode(_ type: String.Type) throws -> String {
-        guard let string = container as? String else {
-            throw typeMismatchError(expcted: type, butGot: container)
-        }
-
-        return string
-    }
-
-    func decode(_ type: Int.Type) throws -> Int {
-        guard let int = container as? Int else {
-            throw typeMismatchError(expcted: type, butGot: container)
-        }
-
-        return int
-    }
-
-    func decode(_ type: Double.Type) throws -> Double {
-        guard let double = container as? Double else {
-            throw typeMismatchError(expcted: type, butGot: container)
-        }
-
-        return double
-    }
+    func decode(_ type: Bool.Type)   throws -> Bool   { return try unbox(type) }
+    func decode(_ type: String.Type) throws -> String { return try unbox(type) }
+    func decode(_ type: Int.Type)    throws -> Int    { return try unbox(type) }
+    func decode(_ type: Double.Type) throws -> Double { return try unbox(type) }
+    func decode(_ type: Data.Type)   throws -> Data   { return try unbox(type) }
+    func decode(_ type: Date.Type)   throws -> Date   { return try unbox(type) }
 
     func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
         switch type {
-        case Date.self:
-            return try decode(type)
-        case Data.self:
-            return try decode(type)
+        case is Date.Type:
+            return try decode(Date.self) as! T
+        case is Data.Type:
+            return try decode(Data.self) as! T
         default:
             return try T(from: self)
         }
-
-        return try T(from: self)
     }
 
     // MARK: SingleValueDecodingContainer (Unsupported types)
 
-    func decode(_ type: Float.Type) throws -> Float { fatalError() }
-    func decode(_ type: Int8.Type)  throws -> Int8 { fatalError() }
-    func decode(_ type: Int16.Type) throws -> Int16 { fatalError() }
-    func decode(_ type: Int32.Type) throws -> Int32 { fatalError() }
-    func decode(_ type: Int64.Type) throws -> Int64 { fatalError() }
-    func decode(_ type: UInt.Type) throws -> UInt { fatalError() }
-    func decode(_ type: UInt8.Type) throws -> UInt8 { fatalError() }
+    func decode(_ type: Float.Type)  throws -> Float  { fatalError() }
+    func decode(_ type: Int8.Type)   throws -> Int8   { fatalError() }
+    func decode(_ type: Int16.Type)  throws -> Int16  { fatalError() }
+    func decode(_ type: Int32.Type)  throws -> Int32  { fatalError() }
+    func decode(_ type: Int64.Type)  throws -> Int64  { fatalError() }
+    func decode(_ type: UInt.Type)   throws -> UInt   { fatalError() }
+    func decode(_ type: UInt8.Type)  throws -> UInt8  { fatalError() }
     func decode(_ type: UInt16.Type) throws -> UInt16 { fatalError() }
     func decode(_ type: UInt32.Type) throws -> UInt32 { fatalError() }
     func decode(_ type: UInt64.Type) throws -> UInt64 { fatalError() }
 
-    // MARK: Internal Encoding
-
-    private func decode(_ type: Data.Type) throws -> Data {
-        fatalError()
-    }
-
-    private func decode(_ type: Date.Type) throws -> Date {
-        fatalError()
-    }
-
     // MARK: Private API
+
+    private func unbox<T>(_ type: T.Type) throws -> T {
+        guard let value = container as? T else {
+            throw typeMismatchError(expcted: type, butGot: container)
+        }
+
+        return value
+    }
 
     private func typeMismatchError<Expected, Actual>(expcted: Expected.Type, butGot actual: Actual) -> DecodingError {
         return DecodingError.typeMismatch(expcted, DecodingError.Context(codingPath: codingPath, debugDescription: "Expected type \(expcted) but got \(type(of: actual)) instead."))
