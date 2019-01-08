415f957f61cfdc3e8e231d5c6c133ae9bc847b2d
Josef Dolezal <pepik.ml@gmail.com>
2019-01-09 00:38:09 +0100

    Add procedure unboxing tests

--- FastRPCSwiftTests/Serialization/FastRPCUnboxerTests.swift
+++ FastRPCSwiftTests/Serialization/FastRPCUnboxerTests.swift
@@ -13,10 +13,18 @@ class FastRPCUnboxerTests: XCTestCase {
 
     func testResponseUnbox() throws {
         // List all test files matching given pattern
-        try bundledFiles().matching("Response/response.*\\.frpc")
+        try bundledFiles().matching("response.*\\.frpc")
             // Read files content
             .map { try Data(contentsOf: $0) }
             // Unbox all test files
+            .forEach { data in
+                XCTAssertNoThrow(try FastRPCSerialization.object(with: data))
+            }
+    }
+
+    func testProcedureUnbox() throws {
+        try bundledFiles().matching("procedure.*\\.frpc")
+            .map { try Data(contentsOf: $0) }
             .forEach { data in
                 XCTAssertNoThrow(try FastRPCSerialization.object(with: data))
                 print(try FastRPCSerialization.object(with: data))
