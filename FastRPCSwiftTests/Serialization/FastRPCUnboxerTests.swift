9eb74ca7c343869277c70dd52c9cb7f20108e63a
Josef Dolezal <pepik.ml@gmail.com>
2019-01-08 23:50:51 +0100

    Add response unboxing tests

---
+++ FastRPCSwiftTests/Serialization/FastRPCUnboxerTests.swift
@@ -0,0 +1,26 @@
+//
+//  FastRPCUnboxerTests.swift
+//  FastRPCSwiftTests
+//
+//  Created by Josef Dolezal on 08/01/2019.
+//  Copyright © 2019 Josef Dolezal. All rights reserved.
+//
+
+import XCTest
+@testable import FastRPCSwift
+
+class FastRPCUnboxerTests: XCTestCase {
+
+    func testResponseUnbox() throws {
+        // List all test files matching given pattern
+        try bundledFiles().matching("Response/response.*\\.frpc")
+            // Read files content
+            .map { try Data(contentsOf: $0) }
+            // Unbox all test files
+            .forEach { data in
+                XCTAssertNoThrow(try FastRPCSerialization.object(with: data))
+                print(try FastRPCSerialization.object(with: data))
+            }
+    }
+
+}
